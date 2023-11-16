f9c507280384b8836aeff8a311895d954bc63963
Joe Rodgers <joerodgers89@outlook.com>
2023-11-16 17:49:47 +0000

    Completed DiaryEntry class and unit tests

--- lib/diary_entry.py
+++ lib/diary_entry.py
@@ -1,3 +1,5 @@
+import math
+
 class DiaryEntry:
     # Public Properties:
     #   title: a string
@@ -8,11 +10,14 @@ class DiaryEntry:
         #   Sets the title and contents properties
         self._title = title
         self._contents = contents
+        self._words = contents.split()
+        self._word_count = len(self._words)
+        self._index = 0
 
     def count_words(self):
         # Returns:
         #   An integer representing the number of words in the contents
-        pass
+        return len(self._contents.split())
 
     def reading_time(self, wpm):
         # Parameters:
@@ -21,7 +26,10 @@ class DiaryEntry:
         # Returns:
         #   An integer representing an estimate of the reading time in minutes
         #   for the contents at the given wpm.
-        pass
+        if wpm <= 0:
+            raise Exception("WPM must be greater than 0")
+        
+        return math.ceil(self.count_words() / wpm)
 
     def reading_chunk(self, wpm, minutes):
         # Parameters:
@@ -35,4 +43,12 @@ class DiaryEntry:
         # If called again, `reading_chunk` should return the next chunk,
         # skipping what has already been read, until the contents is fully read.
         # The next call after that it should restart from the beginning.
-        pass
+        chunk_size = wpm * minutes
+        text_chunk = ' '.join(self._words[self._index:self._index + chunk_size])
+        self._index += chunk_size
+        
+        # Loop round to start reading from the start again if all text read
+        if self._index >= self._word_count:
+            self._index = 0
+            
+        return text_chunk
